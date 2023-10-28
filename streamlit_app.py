13142e2fb67062dd6c85f3c7a1a2090fc6e83051
ihorkalchuk <149180685+ihorkalchuk@users.noreply.github.com>
2023-10-28 13:01:05 +0300

    Err fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Mom''s New Helthy Diner')
 streamlit.header('🥣🍞 Breakfast Menu', divider='rainbow')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥑 'Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
    
