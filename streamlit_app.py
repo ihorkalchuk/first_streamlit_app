cb3589a5c78a5435bc110e7fa34d876a8b59d001
ihorkalchuk <149180685+ihorkalchuk@users.noreply.github.com>
2023-10-28 12:44:24 +0300

    Menu 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Helthy Diner')
+streamlit.header('Breakfast Menu', divider='rainbow')
+streamlit.text('Omega 1 & Blueberry')
+streamlit.text('Kale')
+streamlit.text('Eggs')
