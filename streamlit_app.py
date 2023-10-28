a4722a4fab0a7e51f6fdbc1883cdf531cfdc3fb5
ihorkalchuk <149180685+ihorkalchuk@users.noreply.github.com>
2023-10-28 12:59:47 +0300

    Mom's special menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Helthy Diner')
-streamlit.header('Breakfast Menu', divider='rainbow')
-streamlit.text('Omega 1 & Blueberry')
-streamlit.text('Kale')
-streamlit.text('Eggs')
+streamlit.title('My Mom's New Helthy Diner')
+streamlit.header('🥣🍞 Breakfast Menu', divider='rainbow')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥑 'Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+   
